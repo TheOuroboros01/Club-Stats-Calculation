c0bf74273aab19147e39750a82e0a1b689f82793
TheOuroboros01 <the.ouroboros1@yahoo.com>
2026-04-14 10:31:44 +0530

    Update stats.js

--- stats.js
+++ stats.js
@@ -12,9 +12,9 @@ module.exports = async function runStatsExtractor(page) {
   // ✅ ENTER ALL CLUB URLs HERE
   const clubUrls = [
     //"https://v3.g.ladypopular.com/guilds.php?id=1221", //Loyalty
-    "https://v3.g.ladypopular.com/guilds.php?id=5787",
-    "https://v3.g.ladypopular.com/guilds.php?id=5791",
-    "https://v3.g.ladypopular.com/guilds.php?id=5790",
+    "https://v3.g.ladypopular.com/guilds.php?id=1221",
+    "https://v3.g.ladypopular.com/guilds.php?id=145",
+    "https://v3.g.ladypopular.com/guilds.php?id=3007",
     // add up to 7 (or more)
   ];
 
